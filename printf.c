63db782c5331abbecd428d4a16fc0c9a69e21a53
Ifaol <ifaolhunde@gmail.com>
2023-08-22 08:35:46 -0700

    a function that produces output according to a format updated

--- printf.c
+++ printf.c
@@ -1,43 +1,17 @@
 #include "main.h"
 /**
- *_printf - a custome printf function
- *@format: const char pointer input
+ *_printf_helper_str - a printf helper function for string cases
+ *@s: string input
+ *@chars_written: int input
  *
- *Return: no of chars written
+ *Return: int chars written
  */
-int _printf(const char *format, ...)
+int _printf_helper_str(char *s, int chars_written)
 {
-int chars_written = 0;
 char *null = "(null)";
-const char *ptr = format;
-char *s, c;
-va_list args;
-if (format == NULL)
-{
-return (-1);
-}
-va_start(args, format);
-while (*ptr != '\0')
-{
-if (*ptr == '%')
-{
-ptr++;
-if (*ptr == '\0')
-{
-return (-1);
-}
-if (*ptr == 'c')
-{
-c = (char)va_arg(args, int);
-_putchar(c);
-chars_written++;
-}
-else if (*ptr == 's')
-{
-s = (char *)va_arg(args, char *);
 if (s == NULL)
 {
-while(*null != '\0')
+while (*null != '\0')
 {
 _putchar(*null);
 chars_written++;
@@ -50,15 +24,48 @@ _putchar(*s);
 s++;
 chars_written++;
 }
+return (chars_written);
+}
+#include "main.h"
+/**
+ *_printf - a custome printf function
+ *@format: const char pointer input
+ *
+ *Return: no of chars written
+ */
+int _printf(const char *format, ...)
+{
+int chars_written = 0;
+const char *ptr = format;
+char *s, c;
+va_list args;
+if (format == NULL)
+{
+return (-1);
+}
+va_start(args, format);
+while (*ptr != '\0')
+{
+if (*ptr == '%' && (*(ptr + 1) == 'c' || *(ptr + 1) == 's' || *(ptr + 1) == '\0' || *(ptr + 1) == '%'))
+{
+ptr++;
+if (*ptr == '\0')
+{
+return (-1);
+}
+else if (*ptr == 'c')
+{
+c = (char)va_arg(args, int);
+_putchar(c);
+chars_written++;
+}
+else if (*ptr == 's')
+{
+s = (char *)va_arg(args, char *);
+chars_written = _printf_helper_str(s, chars_written);
 }
 else if (*ptr == '%')
 {
-_putchar(*ptr);
-chars_written++;
-}
-else
-{
---ptr;
 _putchar(*ptr);
 chars_written++;
 }
